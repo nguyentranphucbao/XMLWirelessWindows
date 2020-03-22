9901e2299d122cd04de8a05a6d375d84eae30fce
nguyentranphucbao <nguyentranphucbao@gmail.com>
2020-03-22 12:26:39 +0700

    UP Sunday, March 22, 2020

--- setup.py
+++ setup.py
@@ -3,19 +3,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="xml", # Replace with your own username
+    name="abcXMLcodeCreateFile", # Replace with your own username
     version="0.0.2",
     author="Nguyen Tran Phuc Bao",
     author_email="nguyentranphucbao@gmail.com",
     description="Create, Edit and Change Infomation of XML File",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/nguyentranphucbao/abcXMLcodeCreateFile",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: Windows 8/10/XP, Linux, OS",
+        "Operating System :: OS Independent",
     ],
     python_requires='>=3.5',
 )
